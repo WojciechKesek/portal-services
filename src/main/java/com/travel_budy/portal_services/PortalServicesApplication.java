a6f5fa6dfe15d7b81aae6d03cb28a2bc2fb66557
wkesek <wojtekesek@gmail.com>
2025-11-01 12:47:54 +0100

    inti commit

---
+++ src/main/java/com/travel_budy/portal_services/PortalServicesApplication.java
@@ -0,0 +1,13 @@
+package com.travel_budy.portal_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalServicesApplication {
+
+    static void main(String[] args) {
+		SpringApplication.run(PortalServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel_budy/portal_services/PortalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_budy.portal_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
